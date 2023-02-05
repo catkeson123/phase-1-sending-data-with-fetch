39e3eb1b1982a4bf731ba2d0ee15e4c7206d89de
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-02-05 15:23:47 -0500

    almost complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((object) => {
+      const id = document.createElement("h1");
+      id.innerText = object.id;
+      document.body.appendChild(id);
+    })
+    .catch((error) => {
+      const message = document.createElement("h1");
+      message.innerText = error.message;
+      document.body.appendChild(message);
+    });
+}
